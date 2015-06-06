3e3e71f996532d7dc2aac9c0478a596dbe7bffee
Andre Loureiro <andreloureiroo@gmail.com>
2015-06-06 17:54:51 -0300

    Add chat action to message

--- js/components/chat/ChatMessageInput.jsx
+++ js/components/chat/ChatMessageInput.jsx
@@ -1,17 +1,26 @@
 var React = require('react');
+var ChatActions = require('../../actions/ChatActions.jsx');
 
 var ChatMessageInput = React.createClass({
+
+
 
 	render: function () {
 		
 		return (
 			<div className="row">
 				<div className="col s11">
-					<input type="text" />
+					<input type="text" onKeyDown={ this._onKeyDown } />
 				</div>
 			</div>
 		)
 
+	},
+
+	_onKeyDown: function (e) {
+		if (e.target.keyCode == 13) {
+			ChatActions.addMessage(e.target.value);
+		};
 	}
 
 });
