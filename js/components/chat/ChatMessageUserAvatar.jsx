c95c350ea608f866efa74ef03c127d24b032fc9e
Andre Loureiro <andreloureiroo@gmail.com>
2015-06-06 17:31:03 -0300

    Create user avatar component

---
+++ js/components/chat/ChatMessageUserAvatar.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+
+var ChatMessageUserAvatar = React.createClass({
+
+	getDefaultProps: function () {
+		return {
+			avatarImage: ''
+		}
+	}
+
+	render: function () {
+		return (
+			<div>
+				<img class="circle reponsive-img" src={ this.props.avatarImage } />
+			</div>
+		)
+	}
+
+});
+
+module.exports = ChatMessageUserAvatar;
