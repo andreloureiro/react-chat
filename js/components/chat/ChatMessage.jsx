84cda2700a9d357d3040c7916fae35466ea5c777
Andre Loureiro <andreloureiroo@gmail.com>
2015-06-06 17:35:34 -0300

    Add usar image avatar to chat message

--- js/components/chat/ChatMessage.jsx
+++ js/components/chat/ChatMessage.jsx
@@ -1,11 +1,22 @@
 var React = require('react');
+var ChatMessageUserAvatar = require('./ChatMessageUserAvatar.jsx');
 
 var ChatMessage = React.createClass({
+
+	getDefaultProps: function () {
+		return {
+			userAvatar: '',
+			userMessage: ''
+		}
+	},
 
 	render: function () {
 		
 		return (
-			<div  className="col s12">
+			<div  className="row">
+				<div className="col s3">
+					<ChatMessageUserAvatar avatarImage={ this.props.userAvatar } />
+				</div>
 			</div>
 		)
 
