788ccbff9f58973fa9f4a491c3d17b127e93e0bf
Andre Loureiro <andreloureiroo@gmail.com>
2015-06-06 17:22:28 -0300

    Create ChatStore

--- js/stores/ChatStore.js
+++ js/stores/ChatStore.js
@@ -3,4 +3,50 @@ var Constants = require('../constants/Constants');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 
+var CHANGE_EVENT = 'change';
+
+
 var _messages = [];
+
+function addMessage (message) {
+	_messages.push(message);
+}
+
+
+var ChatStore = assign({}, EventEmitter, {
+
+	getMessages: function () {
+		return _messages
+	}
+
+	emitChange: function () {
+		this.emit(CHANGE_EVENT);
+	},
+
+	addListener: function (callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
+
+	removeListener: function (callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	}
+
+});
+
+
+AppDispatcher.register(function (action) {
+	
+	switch(action.actionType) {
+
+		case Constants.ADD_MESSAGE:
+			addMessage(action.data);
+			ChatStore.emitChange();
+			break;
+
+		default:
+	}
+
+});
+
+
+module.exports = ChatStore;
